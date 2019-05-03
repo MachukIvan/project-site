1e792492bd2b7e50749c52801e62f0b420c5bf34
Ivan <machuk.ivan@yandex.by>
2019-05-03 12:45:49 +0300

    added slider

--- js/script.js
+++ js/script.js
@@ -107,7 +107,6 @@ window.addEventListener('DOMContentLoaded', function () {
     setClock('timer', deadline);
 
     // Modal
-
     let moreBtn = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
         closeBtn = document.querySelector('.popup-close'),
@@ -135,8 +134,7 @@ window.addEventListener('DOMContentLoaded', function () {
     });
     closeBtn.addEventListener('click', close);
 
-    // Form
-
+    // Forms
     let message = {
         loading: 'Загружаю',
         success: 'Скоро мы с вами свяжемся',
@@ -189,5 +187,45 @@ window.addEventListener('DOMContentLoaded', function () {
     contactForm.addEventListener('submit', function (event) {
         event.preventDefault();
         postXHR(contactForm);
+    });
+
+    // Slides
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+        dots.forEach(item => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function changeSlide(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => changeSlide(-1));
+    next.addEventListener('click', () => changeSlide(1));
+
+    dotsWrap.addEventListener('click', event => {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                showSlides(slideIndex = i);
+            }
+        }
     });
 });
